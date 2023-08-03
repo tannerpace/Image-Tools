b022f9e207626cb62a4921c79c65f045d874c59c
TannerB <tannerpace@gmail.com>
2023-08-03 11:00:33 -0400

    fix

--- icon.sh
+++ icon.sh
@@ -1,42 +1,28 @@
 #!/bin/bash
+filepaths=$(osascript <<EOF
+set the_files to choose file with multiple selections allowed with prompt "Please select the images"
+set the_paths to ""
+repeat with a_file in the_files
+    set the_paths to the_paths & POSIX path of a_file & "\n"
+end repeat
+return the_paths
+EOF
+)
 
-# Open Finder and allow the user to select an image
-echo "Please select an image to resize for favicon (PNG, JPG, GIF supported)"
-image_file=$(osascript -e 'tell application "Finder" to ¬
-    POSIX path of (choose file with prompt "Select an image to resize for favicon" of type {"PNG", "jpg", "gif"}))')
-
-# prompts the user to get target to move the files to using the osascript command
-echo "Please select the Next.js project directory to move the files to"
-target_dir=$(osascript -e 'tell application "Finder" to ¬
-   POSIX path of (choose folder with prompt "Select the Next.js root directory")')
-
-# prompt the user if they are using a src directory in their project
-echo "Are you using a src directory in your project? (y/n)"
-read src_dir
-if [ "$src_dir" = "y" ]; then
-    target_dir="$target_dir/src/"
+if [ -z "$filepaths" ]; then
+    echo "No file selected."
+    exit 1
 fi
 
-# Convert image to PNG if necessary
-ext="${image_file##*.}"
-if [ "$ext" != "png" ]; then
-    sips -s format png "$image_file" --out "${image_file%.*}.png"
-    image_file="${image_file%.*}.png"
-fi
+# process each file path
+while IFS= read -r filepath; do
+    
+    sips -Z 32 -s format png "$filepath" --out "${filepath%.*}_favicon.png"
 
-# Resize the image to favicon size using the sips command
-echo "Resizing image to favicon size..."
-
-sips -Z 16 "$image_file" --out "${target_dir}favicon-16x16.png"
-sips -Z 32 "$image_file" --out "${target_dir}favicon-32x32.png"
-sips -Z 48 "$image_file" --out "${target_dir}favicon-48x48.png"
-
-# Convert to ICO using ImageMagick
-convert "${target_dir}favicon-16x16.png" "${target_dir}favicon-32x32.png" "${target_dir}favicon-48x48.png" -colors 256 "${target_dir}favicon.ico"
+    # Convert to ICO using ImageMagick
+    convert "${filepath%.*}_favicon.png" -define icon:auto-resize=16,32 "${filepath%.*}_favicon.ico"
+    
+    open "$(dirname "$filepath")"
+done <<< "$filepaths"
 
 echo "Favicon created successfully."
-
-# Clean up temporary files
-rm "${target_dir}favicon-16x16.png"
-rm "${target_dir}favicon-32x32.png"
-rm "${target_dir}favicon-48x48.png"
